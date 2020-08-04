bd2f78144a582fc3819255dcd2fe0ed645f53762
Tamizh <2162408+thamizh85@users.noreply.github.com>
2020-08-04 22:37:00 +0800

    correct the boolean condition order

--- src/main/scala/scala_99_problems/P34.scala
+++ src/main/scala/scala_99_problems/P34.scala
@@ -1,6 +1,6 @@
 package scala_99_problems
 
 class P34(x: Int) {
-  def totient: Int = Stream.from(1).takeWhile(_ <= x).count(_ % x == 0)
+  def totient: Int = Stream.from(1).takeWhile(_ <= x).count(x % _ == 0)
 
 }
